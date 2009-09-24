417958a97de09f0137a8b5869c1105267af0577e
Chad Woolley <thewoolleyman@gmail.com>
2009-09-24 11:48:29 -0700

    get deploy working

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,33 @@
+require "eycap/recipes"
+
 set :application, "pulse"
 set :repository,  "git@github.com:thewoolleyman/pulse.git"
 set :use_sudo, false
 set :scm, :git
 set :user, 'ci'
+set :git_enable_submodules, 1
 
 role :web, "ci.rubyonrails.org"                          # Your HTTP server, Apache/etc
 role :app, "ci.rubyonrails.org"                          # This may be the same as your `Web` server
 role :db,  "ci.rubyonrails.org", :primary => true # This is where Rails migrations will run
 role :db,  "ci.rubyonrails.org"
 
-before "deploy:restart", "geminstaller"
+after "deploy:symlink", "deploy:geminstaller"
+after "deploy:update_code", "deploy:symlink_configs"
 
-task :geminstaller, :roles => :app do
-  run "sudo geminstaller -c #{current_release}/config/geminstaller.yml"
+namespace :deploy do
+  task :default do
+    deploy.long
+  end
+
+  task :geminstaller, :roles => :app do
+    run "sudo geminstaller -c #{current_release}/config/geminstaller.yml"
+  end
+
+  # Hacks to use Passenger and not Mongrel
+  task :start do  end
+  task :stop do  end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
 end
